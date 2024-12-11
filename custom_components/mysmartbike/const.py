e291d298fd5e3bed7ef7cf1e8759b2d32f908fb6
ReneNulschDE <ReneNulschDE.github@Nulsch.de>
2024-12-11 19:30:59 +0100

    Fix simulator settings

--- custom_components/mysmartbike/const.py
+++ custom_components/mysmartbike/const.py
@@ -1,4 +1,5 @@
 """Constants for the MySmartBike integration."""
+
 from __future__ import annotations
 
 from datetime import timedelta
@@ -12,14 +13,14 @@ DOMAIN = "mysmartbike"
 LOGGER = logging.getLogger(__package__)
 UPDATE_INTERVAL = timedelta(seconds=300)
 
-USE_SIMULATOR = True
+USE_SIMULATOR = False
 API_BASE_URI_SIMULATOR = "http://0.0.0.0:8001"
 
 API_BASE_URI_CLOUD = "https://my-smartbike.com"
 API_BASE_URI = API_BASE_URI_CLOUD if not USE_SIMULATOR else API_BASE_URI_SIMULATOR
 
 USE_PROXY = False
-VERIFY_SSL = False
+VERIFY_SSL = True
 SYSTEM_PROXY: str | None = None if not USE_PROXY else "http://192.168.178.68:9090"
 
 API_USER_AGENT = "ENDUSER/2.1.1 (com.mahle.sbs; build 9; IOS 17.3)"
